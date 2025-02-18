ed024caf1e8438a463dc15f8d79d33f84306ca85
jwz <jwz@jwzdeMacBook-Pro.local>
2025-02-18 10:45:06 +0800

    feat(wenzhuo): update intership and add datastruct dir

---
+++ wenzhuo/Learning_materials/datastruct/skiplist.go
@@ -0,0 +1,25 @@
+package main
+
+type Node struct {
+    data int
+    next *Node
+}
+
+type Element struct {
+    data KV //真正的数据
+    levels []*Element   //存放节点   
+}
+
+
+var ElemA *Element
+var ElemB *Element
+var ElemC *Element
+var ElemE *Element
+var ElemI *Element
+
+func init() {
+    ElemA.levels[0] = ElemB
+    ElemA.levels[1] = ElemC
+    ElemA.levels[2] = ElemE
+    ElemA.levels[3] = ElemI
+}
